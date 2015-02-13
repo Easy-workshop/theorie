25b18233c8df0078426a46d8b45f9c39a375079f
unknown <YADRIHMI@FR003610.corp.capgemini.com>
2015-02-13 11:05:16 +0000

    ajout de table user par le biais de Hibernate

---
+++ java/v3-jdbc_Hibernate/core/src/main/java/com/elephorm/formation/core/app0.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.elephorm.formation.core;
+
+import com.elephorm.formation.core.util.HibernateUtil;
+
+
+public class app0 {
+    public static void main(String[] arg){
+    
+    HibernateUtil.getSessionFactory();
+    
+    }
+    
+}
